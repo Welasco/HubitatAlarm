232ad2b5ac0113d6c8cb022c6a6eb79878a59511
welasco <victor@hepoca.com>
2022-02-08 13:05:11 -0600

    Updated docker images and buildfile

--- Alarm/dockerbuild.sh
+++ Alarm/dockerbuild.sh
@@ -2,37 +2,37 @@
 docker build -t alarm .
 docker tag alarm welasco/hubitatalarm:amd64_1
 docker tag alarm welasco/hubitatalarm:amd64_1.0
-docker tag alarm welasco/hubitatalarm:amd64_1.0.0
+docker tag alarm welasco/hubitatalarm:amd64_1.0.1
 docker login
 docker push welasco/hubitatalarm:amd64_1
 docker push welasco/hubitatalarm:amd64_1.0
-docker push welasco/hubitatalarm:amd64_1.0.0
+docker push welasco/hubitatalarm:amd64_1.0.1
 
 # linux/arm/v7
 docker build -t alarm .
 docker tag alarm welasco/hubitatalarm:armv7_1
 docker tag alarm welasco/hubitatalarm:armv7_1.0
-docker tag alarm welasco/hubitatalarm:armv7_1.0.0
+docker tag alarm welasco/hubitatalarm:armv7_1.0.1
 docker login
 docker push welasco/hubitatalarm:armv7_1
 docker push welasco/hubitatalarm:armv7_1.0
-docker push welasco/hubitatalarm:armv7_1.0.0
+docker push welasco/hubitatalarm:armv7_1.0.1
 
 # linux/arm/v6
 docker build -t alarm .
 docker tag alarm welasco/hubitatalarm:armv6_1
 docker tag alarm welasco/hubitatalarm:armv6_1.0
-docker tag alarm welasco/hubitatalarm:armv6_1.0.0
+docker tag alarm welasco/hubitatalarm:armv6_1.0.1
 docker login
 docker push welasco/hubitatalarm:armv6_1
 docker push welasco/hubitatalarm:armv6_1.0
-docker push welasco/hubitatalarm:armv6_1.0.0
+docker push welasco/hubitatalarm:armv6_1.0.1
 
 # Manifest
 docker manifest create welasco/hubitatalarm:latest \
-    --amend welasco/hubitatalarm:amd64_1.0.0 \
-    --amend welasco/hubitatalarm:armv7_1.0.0 \
-    --amend welasco/hubitatalarm:armv6_1.0.0
+    --amend welasco/hubitatalarm:amd64_1.0.1 \
+    --amend welasco/hubitatalarm:armv7_1.0.1 \
+    --amend welasco/hubitatalarm:armv6_1.0.1
 
 docker manifest create welasco/hubitatalarm:1 \
     --amend welasco/hubitatalarm:amd64_1 \
@@ -44,12 +44,22 @@ docker manifest create welasco/hubitatalarm:1.0 \
     --amend welasco/hubitatalarm:armv7_1.0 \
     --amend welasco/hubitatalarm:armv6_1.0
 
-docker manifest create welasco/hubitatalarm:1.0.0 \
-    --amend welasco/hubitatalarm:amd64_1.0.0 \
-    --amend welasco/hubitatalarm:armv7_1.0.0 \
-    --amend welasco/hubitatalarm:armv6_1.0.0
+docker manifest create welasco/hubitatalarm:1.0.1 \
+    --amend welasco/hubitatalarm:amd64_1.0.1 \
+    --amend welasco/hubitatalarm:armv7_1.0.1 \
+    --amend welasco/hubitatalarm:armv6_1.0.1
 
 docker manifest push welasco/hubitatalarm:latest
 docker manifest push welasco/hubitatalarm:1
 docker manifest push welasco/hubitatalarm:1.0
-docker manifest push welasco/hubitatalarm:1.0.0
+docker manifest push welasco/hubitatalarm:1.0.1
+
+##########################################
+# Additional commands that might be useful
+##########################################
+# remove a manifest for the latest tag
+docker manifest rm welasco/hubitatalarm
+
+# check how a manifest would be pushed to the registry
+docker manifest inspect welasco/hubitatalarm
+
